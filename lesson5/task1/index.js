7cb1a04e336c0233181a68f14c5dc6f1323b4aef
Rostyslav Ostapyak <rostyslav.ostapyak@gmail.com>
2021-07-31 01:30:28 +0300

    lesson 5 try1

---
+++ lesson5/task1/index.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number} firstNum
+ * @param {number} secondNum
+ * @return {number}
+ */
+ function sum(firstNum, secondNum) {
+  // put your code here
+  return firstNum+secondNum
+}
