98edbe3af90a4438d8b0c773a0563912fa13b9e1
Rostyslav Ostapyak <rostyslav.ostapyak@gmail.com>
2021-07-31 00:13:52 +0300

    lesson 4.4 try 1

--- lesson4/task3/index.js
+++ lesson4/task3/index.js
@@ -1,27 +1,26 @@
-const m =2;
-const n =20;
+const m = 2;
+const n = 20;
 
-let result =0;
+let result = 0;
 
-labelLoop: for(let i=m;i<=n;i++){
-
-  if(i%5===0){
+labelLoop: for (let i = m; i <= n; i++) {
+  if (i % 5 === 0) {
     console.log(i);
     continue labelLoop;
   }
-  if(i%2===0 && i %4 === 0){ 
-    result+=i;
-    continue labelLoop;
-  }
-  if(i%3===0){
-    result-=i;
-    continue labelLoop;
-  }
-  if(i%4===0){
-    result*=i;
-    continue labelLoop;
+  if (i % 4 !== 0) {
+    if (i % 2 === 0 && i) {
+      result += i;
+      continue labelLoop;
+    }
   }
 
-
-
+  if (i % 3 === 0) {
+    result -= i;
+    continue labelLoop;
+  }
+  if (i % 4 === 0) {
+    result *= i;
+    continue;
+  }
 }
