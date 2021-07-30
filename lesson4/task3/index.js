3003be07b20fca36a975462acea4a6954dc25ab6
Rostyslav Ostapyak <rostyslav.ostapyak@gmail.com>
2021-07-30 23:50:49 +0300

    lesson4.3 try5

--- lesson4/task3/index.js
+++ lesson4/task3/index.js
@@ -1,22 +1,25 @@
 const m =2;
-const n =8;
+const n =20;
+
 let result =0;
+
 for(let i=m;i<=n;i++){
 
   if(i%5===0){
-    break;
+    console.log(i);
+    continue;
   }
   if(i%2===0 && i %4 === 0){ 
     result+=i;
-    break;
+    continue;
   }
   if(i%3===0){
     result-=i;
-    break;
+    continue;
   }
   if(i%4===0){
     result*=i;
-    break;
+    continue;
   }
 
 
