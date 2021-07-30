ad68dcd46a7bee419cd2e01d66a15f177558ceeb
Rostyslav Ostapyak <rostyslav.ostapyak@gmail.com>
2021-07-31 01:34:29 +0300

    lesson 5.5 try2

--- lesson5/task5/index.js
+++ lesson5/task5/index.js
@@ -1,3 +1,11 @@
-function square (number){
-  return number*number
+function getSum(firstNumber, secondNumber){
+  let sum=0;
+ for (let i = firstNumber;i<=secondNumber;i++){
+   if(i%2===0){
+     console.log(i)
+    sum+=i;
+   }
+ }
+ return sum;
 }
+console.log(getSum(1,8));
