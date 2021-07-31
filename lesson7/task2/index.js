6401b0656378ffd2e3e5934005d4f2b5b18092d4
Rostyslav Ostapyak <rostyslav.ostapyak@gmail.com>
2021-07-31 06:41:21 +0300

    lesson 7

---
+++ lesson7/task2/index.js
@@ -0,0 +1,8 @@
+function sortDesc(array){
+    let resultArray = array.slice()
+    return resultArray.sort(function(a,b){
+        return a-b
+    }).reverse()
+}
+
+console.log(sortDesc([1,25,64,3]))
---
+++ lesson7/task3/index.js
@@ -0,0 +1,4 @@
+const flatArray = arr => {
+    // put your code here
+  };
+  
