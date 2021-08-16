75f168715f4e358e8f05a94a1f2b062137b2036b
Rostyslav Ostapyak <rostyslav.ostapyak@gmail.com>
2021-08-17 00:01:33 +0300

    lesson 8 done

---
+++ lesson8(objects)/task1_createObj/index.js
@@ -0,0 +1,12 @@
+const transformToObject = (arr) => {
+  // put your code here
+  let returnObj = {};
+  arr.forEach((item) => {
+    returnObj[item] = item;
+  });
+  return returnObj;
+};
+
+// examples
+console.log(transformToObject(["a", 17.1, "John Doe"]));
+transformToObject(["a", 17.1, "John Doe"]); // ==> { a: 'a', '17.1': 17.1, 'John Doe': 'John Doe' }
