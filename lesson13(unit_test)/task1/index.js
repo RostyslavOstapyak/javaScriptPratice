7190a38458cfdeac90ca4137351f21be2d29f0b4
Rostyslav Ostapyak <rostyslav.ostapyak@gmail.com>
2021-08-24 16:08:36 +0300

    lesson 13 idk what is going on

--- lesson13(unit_test)/task1/index.js
+++
@@ -1,8 +0,0 @@
-const filterNames = (arr, text) =>
-  arr.filter((names) => names.includes(text) && names.length > 5);
-console.log(
-  filterNames(
-    ["John", "Olivya", "Oleksandr", "Emanuel", "Vanya", "Nastya", "Sanya"],
-    "ya"
-  )
-);
