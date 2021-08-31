d417ecf9159deb1a56b062534416da82f9af4437
Rostyslav Ostapyak <rostyslav.ostapyak@gmail.com>
2021-08-31 19:37:45 +0300

    lesson 21.2 try3

--- lesson21/task2/index.js
+++ lesson21/task2/index.js
@@ -4,5 +4,10 @@ function getTitleElement() {
   console.dir(result);
   return result;
 }
-
+function getInputElement() {
+  const result = document.querySelector('[type="text"]');
+  console.dir(result);
+  return result;
+}
 getTitleElement();
+getInputElement();
