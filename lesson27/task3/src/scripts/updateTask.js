bdc122da2d559c4f4b484d6bc7ea7dc6b102a76f
Rostyslav Ostapyak <rostyslav.ostapyak@gmail.com>
2021-09-07 13:30:20 +0300

    lesson 27.3 try4

--- lesson27/task3/src/scripts/updateTask.js
+++ lesson27/task3/src/scripts/updateTask.js
@@ -2,15 +2,16 @@ import { renderTasks } from "./renderer.js";
 import { getItem, setItem } from "./storage.js";
 
 export const onToggleTask = (e) => {
-  const isCheckbox = e.target.classList.contains("");
+  const isCheckbox = e.target.classList.contains("list__item-checkbox");
 
   if (!isCheckbox) {
     return null;
   }
 
-  const tasksList = getItem("taskList");
+  const tasksList = getItem("tasksList");
   const newTasksList = tasksList.map((task) => {
     if (task.id === e.target.dataset.id) {
+      console.log(e.target.checked);
       const done = e.target.checked;
       return {
         ...task,
