1f666ba6acc95a3d3bc49f2b554ab15bc0bc185a
Rostyslav Ostapyak <rostyslav.ostapyak@gmail.com>
2021-09-07 18:54:47 +0300

    lesson 28 done

---
+++ lesson28/task1/index.js
@@ -0,0 +1,34 @@
+const calc = (initialValue) => {
+  let result = initialValue;
+  const calculator = {
+    add(value) {
+      result += value;
+      return calculator;
+    },
+    mult(value) {
+      result *= value;
+      return calculator;
+    },
+    subtract(value) {
+      result -= value;
+      return calculator;
+    },
+    substract(value) {
+      result -= value;
+      return calculator;
+    },
+    div(value) {
+      result /= value;
+      return calculator;
+    },
+    result() {
+      return result;
+    },
+  };
+
+  return calculator;
+};
+
+const result = calc(3).add(2).mult(4).div(10).substract(5).result();
+
+console.log(result);
