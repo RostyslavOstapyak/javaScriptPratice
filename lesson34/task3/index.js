7cfc9c90e89b043bb2059b5c94c91be6231f4ea5
Rostyslav Ostapyak <rostyslav.ostapyak@gmail.com>
2021-09-17 15:19:59 +0300

    lesson 34.3 try 1

---
+++ lesson34/task3/index.js
@@ -0,0 +1,47 @@
+const baseUrl = "https://61422caa4d16670017ba2c4f.mockapi.io/toDoApi/users";
+const emailField = document.querySelector("#email");
+const userNameField = document.querySelector("#name");
+const passwordField = document.querySelector("#password");
+const submitButton = document.querySelector(".submit-button");
+const formElement = document.querySelector(".login-form");
+const errorElement = document.querySelector(".error-text");
+
+const isValidForm = () => {
+  if (formElement.reportValidity()) {
+    submitButton.disabled = false;
+  } else {
+    submitButton.disabled = true;
+  }
+};
+
+const sendData = (event) => {
+  event.preventDefault();
+
+  const data = {
+    email: emailField.value,
+    userName: userNameField.value,
+    password: passwordField.value,
+  };
+  //   console.log(data);
+  fetch(baseUrl, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json;charset=utf-8",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((response) => response.json())
+    .then((userData) => {
+      emailField.value = "";
+      userNameField.value = "";
+      passwordField.value = "";
+      submitButton.disabled = true;
+      alert(JSON.stringify(userData));
+    })
+    .catch((error) => {
+      errorElement.textContent = "Failed to create user";
+    });
+};
+
+formElement.addEventListener("submit", sendData);
+formElement.addEventListener("input", isValidForm);
